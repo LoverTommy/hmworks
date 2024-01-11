b5c17b762a4cb83de0d0f463ccb46e519cb07d77
peep <sokolenok220@gmail.com>
2024-01-11 09:39:45 +0300

    hmwork4-3-1

--- hmwork4/script.js
+++ hmwork4/script.js
@@ -1,86 +1,125 @@
 'use sctrict'
 
-function ObjStorageFunc() {
-    let storage = {};
 
-    this.addValue = function(key,value) {
-        storage[key] = value;
+//1
+let getWord;
+
+do {
+    getWord = prompt('Введите слово','');
+} while(!getWord)
+
+
+function vowels(word) {
+    let letters = ['О', 'У', 'Ы', 'Э', 'Е', 'Ё', 'И', 'Ю', 'Я','А', 'а', 'о', 'у', 'ы', 'э', 'е', 'ё', 'и', 'ю', 'я'];
+    let numLetters = 0;
+    let wordArr = getWord.split('');
+    var checkedLetter = {};
+
+    function lettersCheck(v) {
+        checkedLetter[v] = v;
     }
+    letters.forEach(lettersCheck);
 
-    this.getValue = function(key) {
-        if(key in storage) {
-            return storage[key];
-        }else {
-            return undefined;
+    function wordLetterCheck(v) {
+        if (v in checkedLetter) {
+            numLetters++;
         }
     }
+    wordArr.forEach(wordLetterCheck);
 
-    this.deleteValue = function(key) {
-        if (key in storage) {
-            delete storage[key];
-            return true;
-        }else {
-            return false;
-        }
-    }
-
-    this.getKeys = function() {
-        return Object.keys(storage);
-    }
+    return numLetters;
 }
 
-document.querySelector('#input').onclick = function () {
-    let namDrink;
-    let recipe;
-    let alco;
-
-    do {
-        namDrink = prompt('Название напитка');
-    } while (!namDrink)
-
-    do {
-        recipe = prompt('Рецепт:');
-    } while (!recipe)
-
-    alco = confirm('Напиток алкогольный?');
-
-    if (alco == true) {
-        alco = 'Да';
-    }else {
-        alco = 'Нет';
-    }
-
-    let info = {'recipe':recipe,'alco':alco};
-
-    drinkStorage.addValue(namDrink,info);
-    alert('Напиток успешно добавлен');
-}
-
-document.querySelector('#get-info').onclick = function () {
-    let namDrink = prompt('Название напитка:');
-    let info = drinkStorage.getValue(namDrink);
-    if(info == undefined) {
-        alert('Такого напитка нет в списке')
-    }else {
-        alert('Напиток:' + ' ' + namDrink + '\n' + 'Репецт:' + ' ' + (info.recipe) + '\n' + 'Алкогольный:' + ' ' + (info.alco));
-    }
-}
-
-document.querySelector('#delete').onclick = function () {
-    let namDrink = prompt('Название напитка:');
-    if (drinkStorage.deleteValue(namDrink) == true) {
-        alert('Напиток успешно удалён');
-    }else {
-        alert('Такого напитка не было в списке');
-    }
-}
-
-document.querySelector('#names').onclick = function () {
-    alert(drinkStorage.getKeys());
-}
+console.log(vowels(getWord));
 
 
-let drinkStorage = new ObjStorageFunc ();
+
+
+
+
+
+
+// function ObjStorageFunc() {
+//     let storage = {};
+
+//     this.addValue = function(key,value) {
+//         storage[key] = value;
+//     }
+
+//     this.getValue = function(key) {
+//         if(key in storage) {
+//             return storage[key];
+//         }else {
+//             return undefined;
+//         }
+//     }
+
+//     this.deleteValue = function(key) {
+//         if (key in storage) {
+//             delete storage[key];
+//             return true;
+//         }else {
+//             return false;
+//         }
+//     }
+
+//     this.getKeys = function() {
+//         return Object.keys(storage);
+//     }
+// }
+
+// document.querySelector('#input').onclick = function () {
+//     let namDrink;
+//     let recipe;
+//     let alco;
+
+//     do {
+//         namDrink = prompt('Название напитка');
+//     } while (!namDrink)
+
+//     do {
+//         recipe = prompt('Рецепт:');
+//     } while (!recipe)
+
+//     alco = confirm('Напиток алкогольный?');
+
+//     if (alco == true) {
+//         alco = 'Да';
+//     }else {
+//         alco = 'Нет';
+//     }
+
+//     let info = {'recipe':recipe,'alco':alco};
+
+//     drinkStorage.addValue(namDrink,info);
+//     alert('Напиток успешно добавлен');
+// }
+
+// document.querySelector('#get-info').onclick = function () {
+//     let namDrink = prompt('Название напитка:');
+//     let info = drinkStorage.getValue(namDrink);
+//     if(info == undefined) {
+//         alert('Такого напитка нет в списке')
+//     }else {
+//         alert('Напиток:' + ' ' + namDrink + '\n' + 'Репецт:' + ' ' + (info.recipe) + '\n' + 'Алкогольный:' + ' ' + (info.alco));
+//     }
+// }
+
+// document.querySelector('#delete').onclick = function () {
+//     let namDrink = prompt('Название напитка:');
+//     if (drinkStorage.deleteValue(namDrink) == true) {
+//         alert('Напиток успешно удалён');
+//     }else {
+//         alert('Такого напитка не было в списке');
+//     }
+// }
+
+// document.querySelector('#names').onclick = function () {
+//     alert(drinkStorage.getKeys());
+// }
+
+
+// let drinkStorage = new ObjStorageFunc ();
 
 
 // class ObjStorageClass {
