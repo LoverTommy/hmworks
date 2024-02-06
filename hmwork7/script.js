177dcaa71993d3f231b45a762bf0db08e2652f47
peep <sokolenok220@gmail.com>
2024-02-06 17:32:31 +0300

    hmwork7-3

--- hmwork7/script.js
+++ hmwork7/script.js
@@ -33,7 +33,47 @@ function boxMouseDown(eo) {
     eo.preventDefault();
     eo = eo || window.event;
     console.log("mouseUp");
-    box.removeEventListener("mousedown", boxMouseDown);
     box.removeEventListener("mousemove", boxMouseMove);
   }
 }
+
+// var box = document.getElementById("box");
+// var boxPosTop = "";
+// var boxPosLeft = "";
+// var mouseTop = "";
+// var mouseLeft = "";
+// box.addEventListener("mousedown", boxMouseDown);
+
+// function boxMouseDown(eo) {
+//   eo = eo || window.event;
+//   boxPosTop = eo.pageY;
+//   boxPosLeft = eo.pageX;
+//   console.log(eo);
+//   console.log(boxPosTop + " " + boxPosLeft);
+//   box.addEventListener("mousemove", boxMouseMove);
+// }
+
+// function boxMouseMove(eo) {
+//   eo.preventDefault();
+//   eo = eo || window.event;
+//   console.log(eo);
+//   mouseTop = eo.pageY;
+//   mouseLeft = eo.pageX;
+//   console.log(mouseTop + " " + mouseLeft);
+
+//   const boxMovePosTop = mouseTop - boxPosTop;
+//   const boxMovePosLeft = mouseLeft - boxPosLeft;
+
+//   return (
+//     (box.style.top = boxMovePosTop + "px"),
+//     (box.style.left = boxMovePosLeft + "px"),
+//     box.addEventListener("mouseup", boxMouseUp)
+//   );
+// }
+
+// function boxMouseUp(eo) {
+//   eo.preventDefault();
+//   eo = eo || window.event;
+//   console.log("mouseUp");
+//   box.removeEventListener("mousemove", boxMouseMove);
+// }
